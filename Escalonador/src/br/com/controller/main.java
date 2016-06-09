6aeccf9c2d5aef0150d061ca17bc197bacc19d30
root <root@nakao-Aspire-E5-573>
2016-06-09 12:14:26 -0300

    Escalonador pronto

--- Escalonador/src/br/com/controller/main.java
+++ Escalonador/src/br/com/controller/main.java
@@ -14,14 +14,26 @@ import br.com.model.Experimento;
 public class main {
     public static void main(String[] args) {
         Arquivo arq = new Arquivo();
-        String in = arq.lerArquivo("src/br/com/files/sjf.exp");
-        String split[]=in.split("\n");
-        String arquivoProcessos=arq.lerArquivo("src/br/com/files/"+split[1]);        
-        ProcessController pl = new ProcessController(arquivoProcessos.trim());
+        try {
+            if(args[0].trim().equals("")){
+                System.out.println("Digitou errado o comando!\ndigite o comando assim no terminal: sudo java -jar Escalonador.jar fifo.exp\nPS:Execute o jar dentro do diretorio src/br/com/files/Escalonador.jar, se quiser executar diretamente \nno projeto do NetBeans, Basta trocar o args[0] por src/br/com/files/fifo.exp por exemplo");
+            }else{
+                String in = arq.lerArquivo(args[0]);
+                String split[]=in.split("\n");
+                String arquivoProcessos=arq.lerArquivo(split[1]);        
+                ProcessController pl = new ProcessController(arquivoProcessos.trim());
+
+                Experimento exp = new Experimento(in,pl);
+                String out = exp.getEscalonador().run();
+                arq.escreverArquivo(exp.getCaminhoArqSaida(), out);
+                System.out.println("Arquivo gerado neste diretorio, seu nome e "+exp.getCaminhoArqSaida());
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Erro ao abrir arquivo,  digite o comando assim no terminal: sudo java -jar Escalonador.jar fifo.exp\nPS:Execute o jar dentro do diretorio src/br/com/files/Escalonador.jar, se quiser executar diretamente no projeto do NetBeans, Basta trocar o args[0] por src/br/com/files/fifo.exp por exemplo");
+        }
         
-        Experimento exp = new Experimento(in,pl);
-        String out = exp.getEscalonador().run();
-        System.out.println(out);
-        arq.escreverArquivo(exp.getCaminhoArqSaida(), out);        
+        
+                
     }
 }
